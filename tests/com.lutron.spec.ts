eb775558c978fa4b1e3f10c2fb0809db7fabafc5
HassanEr-Laptop17 <herrafii80@gmail.com>
2025-04-23 22:22:19 -0400

    Added script to automate Support feature 04/23/10:21pm

--- tests/com.lutron.spec.ts
+++ tests/com.lutron.spec.ts
@@ -1,1 +1,52 @@
+//Import playwright module
 import{test, expect} from '@playwright/test';
+import { assert } from 'console';
+
+
+// write a test 
+test('verify support link ', async ({ page }) => {
+
+    //navigating to the Website
+
+    await page.goto('https://www.lutron.com/us/en');
+    await expect(page).toHaveTitle('Lutron | Intelligent light. Automated Shades. Powerful Controls');
+    await page.getByRole('link', { name: 'Support' }).click();
+
+    // here a new page will be opened and we need to switch to that page
+
+    const pagePromise = page.context().waitForEvent('page');
+    const supportPage = await pagePromise;
+    await supportPage.waitForLoadState();
+    await expect(supportPage.locator('h1')).toHaveText('Lutron Support Center');
+
+    // search for Sunnata Dimmer and sellect Installation Guide
+
+    await supportPage.getByRole('textbox', { name: 'Search model #s or products' }).fill('ST-PRO-N');
+    await supportPage.getByText('ST-PRO-N').click();
+    await supportPage.getByRole('link', { name: 'Installation Guide' }).click();
+    await expect(supportPage.getByText('Sunnata PRO LED+ Dimmer -')).toBeVisible({ timeout: 5000 });
+
+    // Click to open Sunnata PRO LED+ Dimmer - Installation Guide PDF.
+
+    await supportPage.getByRole('list').filter({ hasText: 'Sunnata PRO LED+ Dimmer -' }).getByRole('link');
+
+
+
+
+
+
+
+
+
+
+
+    
+})
+
+//got to url
+
+// click on Support
+
+// search fot Sunnata dimmer product
+
+// click 
