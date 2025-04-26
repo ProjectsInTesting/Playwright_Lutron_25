a458ea6d6bfd57f76ae5b26c9be49d14c1488d20
HassanEr-Laptop17 <herrafii80@gmail.com>
2025-04-26 00:35:37 -0400

    updated code due to website changes 04/26/12:35am

--- tests/com.lutron.spec.ts
+++ tests/com.lutron.spec.ts
@@ -4,6 +4,8 @@ import { test, expect } from '@playwright/test';
 // write a test 
 test('verify support link ', async ({ page }) => {
 
+//test('verify support link ', { tag: ['@Playwright_With_Jenkins'] }, async ({ page }) => {
+
     //navigating to the Website
 
     await page.goto('https://www.lutron.com/us/en');
@@ -19,14 +21,14 @@ test('verify support link ', async ({ page }) => {
     // search for Sunnata Dimmer and sellect Installation Guide
 
     await supportPage.getByRole('textbox', { name: 'Search model #s or products' }).fill('ST-PRO-N');
-    await supportPage.getByText('ST-PRO-N').click();
-    await supportPage.getByRole('link', { name: 'Installation Guide' }).click();
-    await expect(supportPage.getByText('Sunnata PRO LED+ Dimmer -')).toHaveText('Sunnata PRO LED+ Dimmer - Installation Guide');
+    await supportPage.getByRole('link', { name: 'ST-PRO-N Select suggestion' }).click();
+    await supportPage.getByRole('link', { name: 'Open Search item Sunnata ST-' }).click();
 
-    // Click to open Sunnata PRO LED+ Dimmer - Installation Guide PDF.
+    //  a nother page will be opened and we need to switch to that page
+    const supportPage2 = await page.context().waitForEvent('page');
+    await supportPage2.waitForLoadState();
+    await expect(supportPage2).toHaveTitle('Sunnata ST-PRO-N Installation Instructions | Lutron');
+    await supportPage2.getByRole('button', { name: 'Accept' }).click();
 
-    await supportPage.getByRole('list').filter({ hasText: 'Sunnata PRO LED+ Dimmer -' }).getByRole('link').click();
-
-
-
+   
 });
